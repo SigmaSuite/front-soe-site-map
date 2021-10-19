88d5ab15e4d179535fa837e29ea75b86a71d1f0d
Bartosz Sendek <bartosz.sendek@pl.abb.com>
2021-10-19 10:05:48 +0200

    checkout to working microfrontend test template

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('SiteMap app is running!');
+    expect(page.getTitleText()).toEqual('Welcome app is running!');
   });
 
   afterEach(async () => {
