20ac21726417d22f726e3e8901cab42f4b9798e1
senbar <sendek.bartosz@gmail.com>
2021-11-08 18:12:30 +0100

    proof of concept for login func

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,6 +2,7 @@ const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').defaul
 
 module.exports = (angularWebpackConfig, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options)
+  singleSpaWebpackConfig.externals=["@Sigma/auth"];
 
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {auth$, login}  from '@Sigma/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SiteMap';
+  loginStatus= "uninitialized"
+
+  ngOnInit(){
+    login("test", "test");
+    auth$.subscribe(res=> this.loginStatus = res)
+  }
+
+  reload(){
+    console.log(auth$);
+    login("test", "test");
+    auth$.subscribe(res=> this.loginStatus = res)
+  }
 }
